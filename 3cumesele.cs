5e080a7ece5c070613bcb1edd850fd2d7f5411e6
Malik16052 <malikea-bpa202@code.edu.az>
2025-10-07 02:05:44 +0400

    3cu mesele

---
+++ 3cumesele.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("n ededini daxil edin: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    if (n % i == 0)
+    {
+        int bolen = i;
+        Console.WriteLine(i);
+    }
+}
