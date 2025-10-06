2c61b97bd31c9ec852fb198868dd0991af619d02
Malik16052 <malikea-bpa202@code.edu.az>
2025-10-07 02:02:38 +0400

    2ci mesele

---
+++ 2cimesele.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("x ededini daxil edin: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("y ededini daxil edin: ");
+int y = int.Parse(Console.ReadLine());
+int s = 0;
+for (int i = 0; i < x; i++)
+{
+    if (i % y == 0)
+    {
+        s = i;
+    }
+}
+if (s == 0)
+{
+    Console.WriteLine("-1");
+}
+else
+{
+    Console.WriteLine(s);
+}
