e9a79fba1953438f2b82ab268948d9d40cdf16b9
Malik16052 <malikea-bpa202@code.edu.az>
2025-10-07 02:17:59 +0400

    Extra meselesi

---
+++ Extrameselesi.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("n mertebeli ededi daxil edin: ");
+int a = int.Parse(Console.ReadLine());
+int s = 1;
+while (a >= 10)
+{
+    s++;
+    a = a / 10;
+}
+
+Console.WriteLine("verilmis eded " + s + " mertebelidir.");
